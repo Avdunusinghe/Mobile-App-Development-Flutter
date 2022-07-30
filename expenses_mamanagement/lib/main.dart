dbee993963410286f99da26be68d430657cfd8d0
Avdunusinghe <avdunusinghe@gmail.com>
2022-07-30 18:55:19 +0530

    Transaction Card view Implemented

--- expenses_mamanagement/lib/main.dart
+++ expenses_mamanagement/lib/main.dart
@@ -15,7 +15,18 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   final List<Transaction> transactions = [
-    Transaction(id: 't1', title: "Watch", amount: 54.98, date: DateTime.now())
+    Transaction(id: 't1', title: "Watch", amount: 54.98, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: "Call of Duty", amount: 54.98, date: DateTime.now()),
+    Transaction(
+        id: 't3', title: "Hp Laptop", amount: 54.98, date: DateTime.now()),
+    Transaction(
+        id: 't4', title: "Apple x", amount: 54.98, date: DateTime.now()),
+    Transaction(
+        id: 't5',
+        title: "Azure Subscription",
+        amount: 54.98,
+        date: DateTime.now())
   ];
   @override
   Widget build(BuildContext context) {
@@ -33,6 +44,29 @@ class MyHomePage extends StatelessWidget {
                 child: Text('CHART!'),
                 elevation: 5,
               ),
+            ),
+            Column(
+              children: transactions.map((e) {
+                return Card(
+                    child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 2)),
+                          padding: EdgeInsets.all(10),
+                      child: Text(e.amount.toString()),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(e.title),
+                        Text(e.date.toString())
+                      ],
+                    )
+                  ],
+                ));
+              }).toList(),
             )
           ],
         ));
